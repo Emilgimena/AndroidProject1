82c3225aa69ef497e2aadcce535378436f59f5d6
Emilgimena <gimena.emilhenri@gmail.com>
2018-12-05 15:38:50 +0800

    bayot ka

--- app/src/main/java/com/example/ehbgimena/gimena/Main2Activity.java
+++ app/src/main/java/com/example/ehbgimena/gimena/Main2Activity.java
@@ -1,14 +1,38 @@
 package com.example.ehbgimena.gimena;
 
 import android.app.Activity;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.concurrent.TimeUnit;
 
 public class Main2Activity extends Activity {
+    TextView timer;
+    private static final String FORMAT = "%02d";
 
+    int seconds , minutes;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        timer=(TextView) findViewById(R.id.timer);
+
+        new CountDownTimer(4000, 1000) { // adjust the milli seconds here
+
+            public void onTick(long millisUntilFinished) {
+
+                timer.setText(""+String.format(FORMAT,
+                        TimeUnit.MILLISECONDS.toSeconds(millisUntilFinished) - TimeUnit.MINUTES.toSeconds(
+                                TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished))));
+            }
+
+            public void onFinish() {
+
+            }
+        }.start();
     }
 }
